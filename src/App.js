5c857c3595e7298d5fa930b8f1996c538720d1cf
Matt Lobo Cardoso <matheuscardoso1@poli.ufrj.br>
2022-08-01 11:07:28 -0300

    add styled-components, set global styles, change favicon and title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-// import logo from './logo.svg';
-// import './App.css';
+import { Navbar } from "./components";
+import { GlobalStyle } from "./styles";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <GlobalStyle />
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,5 @@
+import * as S from "./styles";
+
+export const Navbar = () => {
+  return <S.NavbarContainer>Navbar!</S.NavbarContainer>;
+};
